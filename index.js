5a58edb70b1671fd262c47b2ff8d116d89951193
Michael McCormack <mcctwin@gmail.com>
2025-02-07 17:35:47 -0600

    7 tests passed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    address: "661 N 1st Street",
+}
+function updateEmployeeWithKeyAndValue(employee, address) {
+    return {
+         ...employee,
+        [address]: "11 Broadway"
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, address) {
+    employee[address] = "12 Broadway"
+    return employee;
+    };
+function deleteFromEmployeeByKey(employee, name) {
+        const newEmployee = delete{...employee[name]};
+        return{newEmployee}         
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name]
+    return employee
+}
